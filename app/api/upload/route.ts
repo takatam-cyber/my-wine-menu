406bbded95c35d797ef41bb2465db426a4dc5711
takatam-cyber <takatam@pieroth.jp>
2026-04-15 19:12:58 +0900

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -7,15 +7,17 @@ export async function POST(req: Request) {
     const file = formData.get('file') as File;
     if (!file) return NextResponse.json({ error: "No file" }, { status: 400 });
 
+    // ファイル名を重複させないための処理
     const fileName = `${Date.now()}-${file.name.replace(/\s+/g, '_')}`;
     
+    // Cloudflare R2に画像を保存
     // @ts-ignore
     await process.env.WINE_IMAGES.put(fileName, file.stream(), {
       httpMetadata: { contentType: file.type }
     });
 
-    // 先ほど確認したあなたのURLをセット
-    const publicBaseUrl = "https://pub-442c135c4d904338a3b472495ae21ddb.r2.dev"; 
+    // 新しく作成されたあなたのURL
+    const publicBaseUrl = "https://pub-8c250d9c7f3844fdbb17adeaae8d32b1.r2.dev"; 
     const imageUrl = `${publicBaseUrl}/${fileName}`;
 
     return NextResponse.json({ url: imageUrl });

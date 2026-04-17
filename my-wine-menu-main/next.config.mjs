57f193c9f6748185cd41eeebe11a40b4e2ca9686
takatam-cyber <takatam@pieroth.jp>
2026-04-17 11:41:58 +0900

    Delete my-wine-menu-main directory

--- my-wine-menu-main/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['node:async_hooks'],
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-};
-
-export default nextConfig;

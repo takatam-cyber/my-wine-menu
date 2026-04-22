ceb40a92b3ade695cfc866ef32c0f7bc4947aa2d
takatam-cyber <takatam@pieroth.jp>
2026-04-22 15:30:12 +0900

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,8 @@
 // lib/auth.ts
 async function getCryptoKey(secret: string) {
   return await crypto.subtle.importKey(
-    "raw",
-    new TextEncoder().encode(secret),
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    ["sign", "verify"]
+    "raw", new TextEncoder().encode(secret),
+    { name: "HMAC", hash: "SHA-256" }, false, ["sign", "verify"]
   );
 }
 
